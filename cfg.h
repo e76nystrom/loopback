9069b1cdb8aae5d25325ffc35e540f349f4efe02
Eric Nystrom <eric.nystrom@yahoo.com>
2026-09-12 10:58:51 -0400

    Fix errors.  Make a copy for cpp conversion.

--- cfg.h
+++ cfg.h
@@ -1,14 +1,13 @@
 #if !defined(CFG_H)
 #define CFG_H
-
-#define PICO_BUILD
+#include <stdint.h>
 
 #if defined(RTK_SEND)
-inline char CLIENT_NAME[] = "cli3";
+#define CLIENT_NAME "cli3"
 #define HOST_NAME CLIENT_NAME
 #endif	/* RTK_SEND */
 
-inline char SERVER_NAME[] = "srv3";
+#define SERVER_NAME "srv3"
 
 #if defined(RTK_RECV)
 #define HOST_NAME SERVER_NAME
